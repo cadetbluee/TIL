eeafcb7c8476c7e042fd2091a9d209d67a2324c9
Ongyeom <ongyeom14@gmail.com>
2024-02-01 09:13:58 +0900

    add

--- Swea/2001.py
+++ Swea/2001.py
@@ -5,15 +5,36 @@ T = int(input())
 for test_case in range(1, T + 1):
     N,M=map(int,input().split())
     array=[list(map(int,input().split())) for _ in range(N)]
-    max_sum=0
+    def dirs(M):
+        result_list=[]
+        for i in range(M):
+            for j in range(M):
+                result_list.extend([(i,j)])
+        return result_list
+
+
+
+    result = 0
     for i in range(N):
         for j in range(N):
-            for n in range(M):
-                for m in range(M):
-                    sum_m=0
-                    if i+M<N and j+M<N:
-                        sum_m+=array[i+n][j+m]
-                    elif
-                    if max_sum<sum_m:
-                        max_sum=sum_m
-    print(f'#{test_case} {max_sum}')
+            sum_m = 0
+            for d in dirs(M):
+                #print(d)
+                ni = i + d[0]  # y방향
+                nj = j + d[1]  # x방향
+                if 0 <= ni < N and 0 <= nj < N:
+                    sum_m+=array[ni][nj]
+            if result<sum_m:
+                result=sum_m
+    # max_sum=0
+    # for i in range(N):
+    #     for j in range(N):
+    #         for n in range(M):
+    #             for m in range(M):
+    #                 sum_m=0
+    #                 if i+M<N and j+M<N:
+    #                     sum_m+=array[i+n][j+m]
+    #                 elif
+    #                 if max_sum<sum_m:
+    #                     max_sum=sum_m
+    print(f'#{test_case} {result}')
