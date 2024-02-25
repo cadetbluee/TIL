a6930ac57bfc3db2d1858b5c00799dc6ab96b1e3
cadetbluee <cadetbluee@icloud.com>
2024-02-25 17:50:49 +0900

    add

--- Swea/4408.py
+++ Swea/4408.py
@@ -3,5 +3,10 @@ sys.stdin=open("4408_input.txt")
 T=int(input())
 for tc in range(1,T+1):
     N=int(input())
+    cnt=[0]*201
     for _ in range(N):
         cur,dep=map(int,input().split())
+        
+        for i in range((min(cur,dep)+1)//2,(max(cur,dep)+1)//2+1):
+            cnt[i]+=1
+    print(f'#{tc}',max(cnt))
