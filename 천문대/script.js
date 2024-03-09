f0310e3794c12b987cd60c8e06325ba676197feb
cadetbluee <cadetbluee@icloud.com>
2024-03-09 20:57:18 +0900

    add

--- 천문대/script.js
+++ 천문대/script.js
@@ -1,35 +1,31 @@
 // JavaScript Document
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+  $(".navi>li")
+    .mouseover(function () {
+      $(this).find(".submenu").stop().slideDown(500);
+    })
+    .mouseout(function () {
+      $(this).find(".submenu").stop().slideUp(500);
+    });
 
-  $('.navi>li').mouseover(function(){        
-	  $(this).find('.submenu').stop().slideDown(500);
-  }).mouseout(function(){
-	  $(this).find('.submenu').stop().slideUp(500);
-  });
-  
-  $('.imgslide a:gt(0)').hide();
-  setInterval(function(){
-	$('.imgslide a:first-child').fadeOut()
-	   .next('a').fadeIn()
-	   .end().appendTo('.imgslide');},3000);
+  $(".imgslide a:gt(0)").hide();
+  setInterval(function () {
+    $(".imgslide a:first-child")
+      .fadeOut()
+      .next("a")
+      .fadeIn()
+      .end()
+      .appendTo(".imgslide");
+  }, 3000);
 
-  $(function(){
-   $('.tabmenu>li>a').click(function(){
-	$(this).parent().addClass("active").siblings().removeClass("active");
-		return false;
-	  });
+  $('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+    var target = $(e.target).attr("href"); // activated tab
   });
-
-  $(".notice li:first").click(function(){
-	$("#modal").addClass("active");
+  $(".notice li:first").click(function () {
+    $("#modal").addClass("active");
   });
-  $(".btn").click(function(){
-	$("#modal").removeClass("active");
-  });    
-	     
+  $(".btn").click(function () {
+    $("#modal").removeClass("active");
+  });
 });
-
-
-
-
