d6f3730c256eb4ccd0a38faa4ee32ae08534718d
cadetbluee <cadetbluee@icloud.com>
2024-03-23 11:53:38 +0900

    add

--- Beakjoon/풍선팡.py
+++ Beakjoon/풍선팡.py
@@ -1,13 +1,10 @@
+from collections import deque
 N=int(input())
 arr=list(map(int,input().split()))
-i=1
-ans=[]
-while len(ans)<N:
-    ans.append(i)
-    if i-2<0:
-        i=len(arr)-1
-    elif i-2>len(arr)-1:
-        i=2
-        
-    i=i+arr.pop(i-2)
-print(ans)
+Q=deque()
+Q.append(1)
+while Q:
+    num=Q.popleft()
+    Q.append(arr.pop(num-1))
+    print(Q)
+
