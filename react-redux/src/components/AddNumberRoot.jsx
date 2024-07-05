99b19556665aec55d2f3347b1486dd3b266cbab4
cadetbluee <cadetbluee@icloud.com>
2024-07-05 17:01:54 +0900

    add

---
+++ react-redux/src/components/AddNumberRoot.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import AddNumber from "../components/AddNumber";
+export default class AddNumberRoot extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+        <AddNumber
+          onClick={function (size) {
+            this.props.onClick(size);
+          }.bind(this)}
+        ></AddNumber>
+      </div>
+    );
+  }
+}
---
+++ react-redux/src/components/DisplayNumber.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class DisplayNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Display Number</h1>
+
+        <input type="text" value={this.props.number} readOnly />
+      </div>
+    );
+  }
+}
